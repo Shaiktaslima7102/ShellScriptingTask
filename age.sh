35b62662484847abd68595994c033a2d6c7c601c
Shaiktaslima7102 <shaiktaslima710@gmail.com>
2024-10-28 02:59:39 +0000

    made changes in age file

--- age.sh
+++ age.sh
@@ -5,8 +5,8 @@
         echo "Category: Child"
     elif (( age >= 13 && age <= 19 )); then
         echo "Category: Teenager"
-##    elif (( age > 60 )); then 
-##	echo "Category: Oldage "
+    elif (( age > 60 )); then 
+	echo "Category: Oldage "
     else
         echo "Category: Adult "
     fi
